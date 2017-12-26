73cf7aee672de00d23a413a29d4dd337ce0b0685
Karthik Kamarthi <karthikkbs@gmail.com>
2017-12-26 16:43:42 +0530

    some cosmotic modifications
    some cosmotic modifications

--- employee-zuul-client/src/main/java/example/springcloud/poc/EmployeeZuulClientApplication.java
+++ employee-zuul-client/src/main/java/example/springcloud/poc/EmployeeZuulClientApplication.java
@@ -1,4 +1,4 @@
-package com.infomover.poc;
+package example.springcloud.poc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- employee-zuul-client/src/main/java/example/springcloud/poc/EmployeeZuulFilter.java
+++ employee-zuul-client/src/main/java/example/springcloud/poc/EmployeeZuulFilter.java
@@ -1,4 +1,4 @@
-package com.infomover.poc;
+package example.springcloud.poc;
 
 import javax.servlet.http.HttpServletRequest;
 
